5b5e0b1cf6672d93eece1cfb9c40e4b41371e164
agnenevulyte <you@example.com>
2020-02-23 19:47:20 +0000

    adding toggle, but need to work on that

--- src/app.js
+++ src/app.js
@@ -1,85 +1,90 @@
 // source goes here
 const app = function() {
-  console.log('hello from app!');
+	// table
+	var lastClicked;
+	var grid = clickableGrid(4, 3, function(el, row, col, i) {
+		// console.log("You clicked on item #:",i);
+		// console.log("row #:",row);
+		// console.log("col #:",col);
 
-  // table
-  var lastClicked;
-  var grid = clickableGrid(4,3,function(el,row,col,i){
-      // console.log("You clicked on item #:",i);
-      // console.log("row #:",row);
-      // console.log("col #:",col);
+		// multiples
+		var n = 12;
+		var clickedNum = i;
 
+		// array of numbers
+		var arrOfNums = [];
+		for (var m = clickedNum; m <= n; m++) {
+			arrOfNums.push(m);
+		}
 
-      // multiples
-      var n = 12;
-      var clickedNum = i;
+		// finding multiples
+		function multiplesOf(numbers, number) {
+			// add second argument
+			var multiples = []; // change to array (so that we can store multiple numbers - not just one multiple)
+			for (var j = 0; j < numbers.length; j++) {
+				if (numbers[j] % number === 0) {
+					// divide by the number
+					multiples.push(numbers[j]); // add the current multiple found to the multiples array
+				}
+			}
+			return multiples;
+		}
 
-      // array of numbers
-      var arrOfNums = [];
-      for (var m = clickedNum; m<=n; m++) {
-        arrOfNums.push(m);
-      }
+		console.log(multiplesOf(arrOfNums, clickedNum));
+		var myArr = multiplesOf(arrOfNums, clickedNum);
 
-      // finding multiples 
-      function multiplesOf(numbers, number) { // add second argument
-        var multiples = []; // change to array (so that we can store multiple numbers - not just one multiple)
-        for (var j = 0; j < numbers.length; j++) {
-          if (numbers[j] % number === 0) { // divide by the number
-            multiples.push(numbers[j]); // add the current multiple found to the multiples array
-          }
-        }
-        return multiples;
-      }
-      
-      console.log(multiplesOf(arrOfNums, clickedNum));
-      var myArr = multiplesOf(arrOfNums,clickedNum);
-      
-      el.className='clicked';
-      if (lastClicked) lastClicked.className='';
-      lastClicked = el;
-      
+		el.className = 'clicked multiples';
+		if (lastClicked) {
+			lastClicked.className = '';
+		}
+		lastClicked = el;
+		console.log('lastClicked------------', lastClicked);
+		addClassToMultiples();
 
-      //https://stackoverflow.com/questions/36313778/adding-class-to-specific-cells-of-a-html-table
+		//https://stackoverflow.com/questions/36313778/adding-class-to-specific-cells-of-a-html-table
+		function addClassToMultiples() {
+			var tds = document.querySelectorAll('.grid td');
+			//iterate over each td
+			for (var i = 0; i < n; i++) {
+				var text = tds[i].innerText;
+				myArr.forEach(function(value) {
+					if (text === value.toString()) {
+						//add your class to the element containing this text
+						console.log('tds[i]', tds[i]);
+						tds[i].classList.toggle('multiples');
+						tds[clickedNum - 1].classList.remove('multiples');
+					}
+				});
+			}
+		}
+	});
 
-      // var tds = document.querySelectorAll(".grid td");
-      // //iterate over each td
-      // for (var i = 0; i < myArr.length; i++) {
-      //   var text = tds[i].innerText;
-      //   //check for your target text
-      //   if (text === "X2") {
-      //     //add your class to the element containing this text
-      //     tds[i].classList.add("X2");
-      //   }
-      // }
-  });
+	document.body.appendChild(grid);
 
-
-
-  document.body.appendChild(grid);
-      
-  function clickableGrid( rows, cols, callback ){
-      var i=0;
-      var grid = document.createElement('table');
-      grid.className = 'grid';
-      for (var r=0;r<rows;++r){
-          var tr = grid.appendChild(document.createElement('tr'));
-          console.log('tr', tr.rowIndex)
-          for (var c=0;c<cols;++c){
-              var cell = tr.appendChild(document.createElement('td'));
-              console.log('td', cell.cellIndex)
-              cell.innerHTML = ++i;
-              cell.addEventListener('click',(function(el,r,c,i){
-                  return function(){
-                      callback(el,r,c,i);
-                  }
-              })(cell,r,c,i),false);
-          }
-      }
-      return grid;
-  }
-
+	function clickableGrid(rows, cols, callback) {
+		var i = 0;
+		var grid = document.createElement('table');
+		grid.className = 'grid';
+		for (var r = 0; r < rows; ++r) {
+			var tr = grid.appendChild(document.createElement('tr'));
+			// console.log('tr', tr.rowIndex);
+			for (var c = 0; c < cols; ++c) {
+				var cell = tr.appendChild(document.createElement('td'));
+				// console.log('td', cell.cellIndex);
+				cell.innerHTML = ++i;
+				cell.addEventListener(
+					'click',
+					(function(el, r, c, i) {
+						return function() {
+							callback(el, r, c, i);
+						};
+					})(cell, r, c, i),
+					false
+				);
+			}
+		}
+		return grid;
+	}
 };
 
 export default app;
-
-
