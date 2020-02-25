87105fa4e382929480376412a7d73ff9e7bd9c21
agnenevulyte <you@example.com>
2020-02-25 22:05:11 +0000

    test planning

--- test/app.test.js
+++ test/app.test.js
@@ -5,3 +5,12 @@ describe('app', function() {
     expect(true).toBe(true);
   });
 });
+
+// does the table exist
+// does it have 144 mini tables
+
+// 1. verify output multiplier is valid - what means calculate a multiple of something
+// 2. for example - verify the highest multiple 72, and it's multiple 144
+// store 72, the 144 is an answer - store in an array
+// 3. for example - verify the highest multiple (72-yellow), and it's multiple (144 green)
+// 4. if the one is yellow, then all the others are green
